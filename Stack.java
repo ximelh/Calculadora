32f914670e1bc99843b95473081439efb1ff7a12
ximelh <ximenaloarca@gmail.com>
2022-02-16 15:06:28 -0600

    Metodos de Stack

--- Stack.java
+++ Stack.java
@@ -1,3 +1,14 @@
+
 public interface Stack <E> {
+
+    public void add(E item);
+
+    public E remove();
+
+    public E peek();
+
+    public boolean empty();
     
+    public int size();
+
 }
--- StackList.java
+++ StackList.java
@@ -1,3 +1,33 @@
 public class StackList<E> extends AStack<E> {
+
+    @Override
+    public void add(E item) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public E remove() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public E peek() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean empty() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
     
 }
