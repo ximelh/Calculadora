d152a23e878e07859e693a480f20d02e4b334473
sofiaesc07 <esc20489@uvg.edu.gt>
2022-02-18 00:22:17 -0600

    Agrege la clase List y la clase ListDouble

--- List.java
+++ List.java
@@ -6,5 +6,22 @@
 
 
 public interface List<E> {
-    
+
+     public void push(String item);
+	   // La expresion se agrega a la lista
+	   
+	   public E pop();
+	   // La expresión se remueve de la lista y se retorna el valor de dicha funcion. 
+
+	   public E peek();
+	   // Se devuelve la ultima expresión que se encuentre en la lista
+	   
+	   public boolean empty();
+	   // Devuelve si la lista se enecuentra vacia o no
+	   
+	   public int size();
+	   // Devuelve la cantidad de elementos dentro de la lista
+
+	   public Double resolver(String input);
+	   // Devuelve el calculo resuelto
 }
